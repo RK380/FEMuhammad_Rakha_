46c030891389473cf94679b9857a961720545f7b
RK380 <rakhachips2@gmail.com>
2023-03-09 10:07:03 +0700

    updt index button and global css

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
 
-export default function Button() {
+export default function Button({type,isLoading}:Props) {
+
+    const Loading = ()=>{
+        return (
+            <div className='flex justify-center'>
+                <div className='animate-spin rounded-full h-5 w-5 border-b-2 border-white'></div>
+            </div>
+        )
+    }
+
   return (
-    <button className='bg-white text-white hover:bg-zinc-800 p-[20px] aspect-square'>
+    <button type={type} className='bg-white text-white hover:bg-zinc-800 p-[20px] aspect-square'>
+        {isLoading ? <Loading /> : <MagnifyingGlassIcon className='w-6 h-6'/>}
         Search
-        {/* <MagnifyingGlassIcon className='w-6 h-6'/> */}
     </button>
   )
+}
+
+interface Props {
+    type?: 'button' | 'submit' | 'reset' | undefined;
+    isLoading?: boolean;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Page from '../../components/Page'
 import Button from '../../components/Button'
+import ListUser from '../../components/ListUser'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,9 +17,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      
       <main className={styles.main}>
         <div className={styles.description}>
-          
           <div>
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -57,17 +58,14 @@ export default function Home() {
             />
           </div>
         </div>
-
-        <div className={styles.grid}>
-        {/* className={styles.card} */}
-            <h2 className={inter.className}>
-              Hunt <span>-&gt;</span>
-            </h2>
-            <form>
-              <Page />
-              <Button />
-            </form>
+        <div className={styles.card}>
+          <form className='flex place-self-center space-x-3'>
+            <Page />
+            <Button type='submit' isLoading={false} />
+            <ListUser />
+          </form>
         </div>
+
       </main>
     </>
   )
